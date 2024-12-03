557efba3fba70beb16639c6675a1537556419cb1
Your Name <you@example.com>
2024-12-03 02:25:55 -0800

    first commit

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv').config();
 
-mongoose.connect("mongodb://127.0.0.0:27017/crudapp");
+const mongodb = mongoose.connect("mongodb://127.0.0.1:27017/crudapp");
 
 const userSchema = mongoose.Schema({
     image: String,
